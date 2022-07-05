ed1ba40aca5fd7ade9d77b2276ad672697497516
Anderson Adolfo <anderson.adolfo1998@gmail.com>
2022-07-05 01:04:06 -0300

    feat: created component header

--- App.tsx
+++ App.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+
 import theme from './src/global/styles/theme';
 
 import { Dashboard } from './src/screens/Dashboard';
 
+import * as SplashScreen from 'expo-splash-screen';
+
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+
+  const [fonstLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if (!fonstLoaded) {
+    return null;
+  }
+
+  SplashScreen.hideAsync();
+
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />

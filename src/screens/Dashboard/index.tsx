95e9f3e414ae32b275cca637306139cc63f41993
Anderson Adolfo <anderson.adolfo1998@gmail.com>
2022-07-05 01:40:10 -0300

    feat: created elements in header component

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
 
+import { } from '@expo/vector-icons';
+
 import {
     Container,
-    Header
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
 } from './styles';
 
 export function Dashboard() {
     return (
         <Container>
-            <Header />
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo
+                            source={{ uri: 'https://github.com/AndersonAslap.png' }}
+                        />
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Anderson</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }

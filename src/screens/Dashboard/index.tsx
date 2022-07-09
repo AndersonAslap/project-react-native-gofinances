37d55d148b7c89dc740839f736db33a646c7ec0c
Anderson Adolfo <anderson.adolfo1998@gmail.com>
2022-07-08 23:48:35 -0300

    feat: create component higlihtcards

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -10,7 +10,8 @@ import {
     User,
     UserGreeting,
     UserName,
-    Icon
+    Icon,
+    HighlightCards
 } from './styles';
 
 export function Dashboard() {
@@ -35,7 +36,13 @@ export function Dashboard() {
 
             </Header>
 
-            <HighlightCard />
+            <HighlightCards>
+                <HighlightCard />
+                <HighlightCard />
+                <HighlightCard />
+            </HighlightCards>
+
+
         </Container>
     )
 }
